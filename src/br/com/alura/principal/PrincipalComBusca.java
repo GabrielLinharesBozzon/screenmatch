b213781bdb13fcf676b80f552e3311716dfffe49
Gabriel <44560145+GabrielLinharesBozzon@users.noreply.github.com>
2026-08-03 18:12:43 -0300

    #

--- src/br/com/alura/principal/PrincipalComBusca.java
+++ src/br/com/alura/principal/PrincipalComBusca.java
@@ -1,8 +1,21 @@
 package br.com.alura.principal;
 
-public class PrincipalComBusca {
-    static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class PrincipalComBusca {
+    static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest requeest = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?t=lord_of_the_rings&apikey=2db9c1f8"))
+                .build();
+        HttpResponse<String> response = client
+                .send(requeest,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
 
 
         
